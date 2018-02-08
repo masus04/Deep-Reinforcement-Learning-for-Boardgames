4224858c6c1aba4464a86289daa3bd32f12481c1
masus04 <thomas_st@hotmail.com>
2018-02-08 03:14:03 +0100

    Trying out the most obvious solution

--- modules.py
+++ modules.py
@@ -5,10 +5,11 @@ class LegalSoftMax(Module):
     
     def __init__(self):
         super(LegalSoftMax, self).__init__()
+        self.HARD_LIMIT = 150
 
     def forward(self, input, legal_map):
 
-        x = input / input.sum()
+        x = input.tanh()
         x = x.exp()
         x = x * legal_map
         x = x / x.sum()
