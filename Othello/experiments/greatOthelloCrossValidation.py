c88b4eb3cab07657301194fb9700ddf1babf4ce6
masus04 <thomas_st@hotmail.com>
2018-05-04 01:21:25 +1000

    Minor corrections

--- Othello/experiments/greatOthelloCrossValidation.py
+++ Othello/experiments/greatOthelloCrossValidation.py
@@ -27,18 +27,21 @@ class GreatTTTCrossValidation(OthelloBaseExperiment):
             experiment = TrainACPlayerVsBest(games=GAMES, evaluations=EVALUATIONS, pretrained_player=player)
             print("\n|| ----- Running %s with %s ----- ||" % (experiment, player))
             experiment.run(lr=LR, batch_size=1)
+            experiment.reset()
 
         # BASELINE
         for player in [FCBaselinePlayer(LR), LargeFCBaselinePlayer(LR)]:
             experiment = TrainBaselinePlayerVsBest(games=GAMES, evaluations=EVALUATIONS, pretrained_player=player)
             print("\n|| ----- Running %s with %s ----- ||" % (experiment, player))
             experiment.run(lr=LR, batch_size=1)
+            experiment.reset()
 
         # REINFORCE
         for player in [FCReinforcePlayer(LR), LargeFCReinforcePlayer(LR)]:
             experiment = TrainReinforcePlayerVsBest(games=GAMES, evaluations=EVALUATIONS, pretrained_player=player)
             print("\n|| ----- Running %s with %s ----- ||" % (experiment, player))
             experiment.run(lr=LR, batch_size=1)
+            experiment.reset()
 
 
 if __name__ == '__main__':
