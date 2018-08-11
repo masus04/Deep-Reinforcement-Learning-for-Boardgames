0071240d53b3bbaf2bf408fc5bc0f092fde89b13
masus <thomas_st@hotmail.com>
2018-08-11 23:15:05 +0200

    Preparation

--- Othello/experiments/trainBaselinePlayerVsTraditionalOpponent.py
+++ Othello/experiments/trainBaselinePlayerVsTraditionalOpponent.py
@@ -74,9 +74,9 @@ if __name__ == '__main__':
 
     for i in range(ITERATIONS):
         print("Iteration %s/%s" % (i + 1, ITERATIONS))
-        GAMES = 5000000
+        GAMES = 2000000
         EVALUATIONS = GAMES//1000
-        LR = random()*1e-9 + 1e-4  # uniform(1e-2, 1e-4)
+        LR = random()*1e-9 + 5e-5  # uniform(1e-2, 1e-4)
 
         PLAYER = None  # Experiment.load_player("ReinforcePlayer using 3 layers pretrained on legal moves for 1000000 games.pth")
         OPPONENT = None  # ExperiencedPlayer(deterministic=True)
